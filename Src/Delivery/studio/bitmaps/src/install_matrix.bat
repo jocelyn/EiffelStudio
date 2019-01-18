5c9f705058476cf7c58447584d940e9e1dd45e21
Jocelyn Fiat <git@djoce.net>
2019-01-18 18:18:19 +0100

    updated

--- Src/Delivery/studio/bitmaps/src/install_matrix.bat
+++ Src/Delivery/studio/bitmaps/src/install_matrix.bat
@@ -1,3 +1,3 @@
-copy icons_16x16.png %ISE_EIFFEL%\studio\bitmaps\png\16x16.png
-copy small_12x12.png %ISE_EIFFEL%\studio\bitmaps\png\12x12.png
-copy mini_10x10.png %ISE_EIFFEL%\studio\bitmaps\png\10x10.png
+copy icons/icons_16x16.png %ISE_EIFFEL%\studio\bitmaps\png\16x16.png
+copy small/small_12x12.png %ISE_EIFFEL%\studio\bitmaps\png\12x12.png
+copy mini/mini_10x10.png %ISE_EIFFEL%\studio\bitmaps\png\10x10.png
